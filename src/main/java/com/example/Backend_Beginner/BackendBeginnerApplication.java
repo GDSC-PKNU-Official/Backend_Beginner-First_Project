963370245d95bb36be17074409bad7ff95a5d39c
Ssechan <tpcksrns@gmail.com>
2024-06-05 02:52:25 +0900

    spring_setting

---
+++ src/main/java/com/example/Backend_Beginner/BackendBeginnerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Backend_Beginner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBeginnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBeginnerApplication.class, args);
+	}
+
+}
