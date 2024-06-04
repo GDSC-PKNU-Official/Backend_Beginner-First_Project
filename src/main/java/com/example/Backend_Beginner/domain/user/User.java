13d8c74c7a9a9cc8e8f78cd24a36197b3006c981
Ssechan <tpcksrns@gmail.com>
2024-06-05 04:38:54 +0900

    Feat#0: Create User

---
+++ src/main/java/com/example/Backend_Beginner/domain/user/User.java
@@ -0,0 +1,15 @@
+package com.example.Backend_Beginner.domain.user;
+
+public class User {
+
+    private String name;
+    private Integer age;
+
+    public User(String name, Integer age) {
+        if(name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name cannot be null or empty");
+        }
+        this.name = name;
+        this.age = age;
+    }
+}
